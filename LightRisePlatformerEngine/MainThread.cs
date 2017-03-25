8259ed701ed9cea5c5b19192e21800a934fe9250
NickLatkovich <nicklatkovich@gmail.com>
2017-03-25 17:58:55 +0300

    Animation of running

--- LightRisePlatformerEngine/MainThread.cs
+++ LightRisePlatformerEngine/MainThread.cs
@@ -6,25 +6,22 @@ using LightRise.BaseClasses;
 using LightRise.WinUtilsLib;
 using System;
 
-namespace LightRise.Main
-{
+namespace LightRise.Main {
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class MainThread : Game
-    {
+    public class MainThread : Game {
 
         GraphicsDeviceManager Graphics;
         SpriteBatch SpriteBatch;
         SpineObject spineObj;
 
-        RenderTarget2D[] Renders;
+        RenderTarget2D[ ] Renders;
         public Map Map { get; protected set; }
         Camera Cam;
         Player Player;
 
-        public MainThread()
-        {
+        public MainThread( ) {
             Graphics = new GraphicsDeviceManager(this);
             DisplayMode displayMode = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode;
             IsMouseVisible = true;
@@ -59,20 +56,18 @@ namespace LightRise.Main
             SimpleUtils.Init(GraphicsDevice);
             // TODO: Renders will be used for more fust drawing of the background... Later
             Renders = new RenderTarget2D[4];
-            for (uint i = 0; i < Renders.Length; i++)
-            {
+            for (uint i = 0; i < Renders.Length; i++) {
                 Renders[i] = new RenderTarget2D(GraphicsDevice, Graphics.PreferredBackBufferWidth, Graphics.PreferredBackBufferHeight);
             }
 
-            base.Initialize();
+            base.Initialize( );
         }
 
         /// <summary>
         /// LoadContent will be called once per game and is the place to load
         /// all of your content.
         /// </summary>
-        protected override void LoadContent()
-        {
+        protected override void LoadContent( ) {
             // Create a new SpriteBatch, which can be used to draw textures.
             SpriteBatch = new SpriteBatch(GraphicsDevice);
             spineObj = new SpineObject(GraphicsDevice, "Sample", 1, new Vector2(20, 10));
@@ -84,8 +79,7 @@ namespace LightRise.Main
         /// UnloadContent will be called once per game and is the place to unload
         /// game-specific content.
         /// </summary>
-        protected override void UnloadContent()
-        {
+        protected override void UnloadContent( ) {
             // TODO: Unload any non ContentManager content here
         }
 
@@ -94,12 +88,11 @@ namespace LightRise.Main
         /// checking for collisions, gathering input, and playing audio.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Update(GameTime gameTime)
-        {
-            StepState State = new StepState(gameTime, Keyboard.GetState(), Mouse.GetState());
+        protected override void Update(GameTime gameTime) {
+            StepState State = new StepState(gameTime, Keyboard.GetState( ), Mouse.GetState( ));
 
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || State.Keyboard.IsKeyDown(Keys.Escape))
-                Exit();
+                Exit( );
 
             float cam_spd = 0.1f;
             float dx = (State.Keyboard.IsKeyDown(Keys.Right) ? cam_spd : 0) - (State.Keyboard.IsKeyDown(Keys.Left) ? cam_spd : 0);
@@ -117,13 +110,12 @@ namespace LightRise.Main
         /// This is called when the game should draw itself.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Draw(GameTime gameTime)
-        {
+        protected override void Draw(GameTime gameTime) {
             GraphicsDevice.Clear(Color.Black);
 
             //SpriteBatch.Begin(transformMatrix: Matrix.CreateOrthographic(Cam.Scale.X, Cam.Scale.Y, -0.1f, 1f));
             //SpriteBatch.Begin(transformMatrix: Matrix.CreateOrthographicOffCenter(new Rectangle((int)(Cam.Position.X - Cam.Scale.X / 2), (int)(Cam.Position.Y - Cam.Scale.Y / 2), (int)Cam.Scale.X, (int)Cam.Scale.Y), 1f, 1000f));
-            SpriteBatch.Begin();
+            SpriteBatch.Begin( );
             Map.Draw(SpriteBatch, Cam);
             SpriteBatch.End( );
             Player.Draw(SpriteBatch, Cam);
