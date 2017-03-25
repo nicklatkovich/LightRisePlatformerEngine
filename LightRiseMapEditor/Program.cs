e2c7fd047209923cf4fdbec583ee7d50e3aefe92
NickLatkovich <nicklatkovich@gmail.com>
2017-03-25 03:03:35 +0300

    MapEditor Init

---
+++ LightRiseMapEditor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LightRiseMapEditor {
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+
+        public static MainThread MainThread { get; private set; }
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (MainThread = new MainThread())
+                MainThread.Run();
+        }
+    }
+#endif
+}
